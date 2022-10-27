c3cdf696a261ee106ed680bda627c152b73a12d5
eborhan <106884905+eborhan@users.noreply.github.com>
2022-10-27 23:14:41 +0300

    Initial commit

---
+++ src/test/java/Teacher.java
@@ -0,0 +1,10 @@
+public class Teacher {
+    String name, phone, branch;
+
+    public Teacher(String name, String phone, String branch) {
+        this.name = name;
+        this.phone = phone;
+        this.branch = branch;
+    }
+
+}
